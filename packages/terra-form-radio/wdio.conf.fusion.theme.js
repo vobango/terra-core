b0fd6d96bfb8eb8d42e9593d2f7901a73295de1c
Derek Yu <yuderekyu@gmail.com>
2019-08-01 10:23:05 -0500

    update wdio scripts

---
+++ packages/terra-form-radio/wdio.conf.fusion.theme.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const wdioConf = require('../../node_modules/terra-toolkit/config/wdio/wdio.conf');
+//const themeHelpers = require('terra-toolkit/lib/wdio/services/TerraCommands/theme-helpers');
+
+const config = {
+  ...wdioConf.config,
+  specs: [path.join(__dirname, 'test*', 'wdio', '**', '*-spec.js')],
+  baseScreenshotDir: './tests/wdio/__snapshots__',
+  before: function () {
+//    themeHelpers.setTheme('orion-fusion-theme');
+    global.browser.execute(`document.documentElement.classList.add('${themeSelector}')`);
+  }
+};
+
+exports.config = config;
