db6da449c7e451a8454b2df7e5cff4a1231be599
Stephen Esser <Stephen.Esser@Cerner.com>
2019-10-04 16:22:19 -0500

    Added a script that aggregates changelogs together by date

---
+++ scripts/release-notes/release-notes.js
@@ -0,0 +1,37 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const fs = require('fs');
+const glob = require('glob');
+
+// This assumes the current date. For testing this can be hard coded to something like October 3, 2019.
+// Might investigate taking a date as an argument.
+const today = new Date().toLocaleString('en-us', { month: 'long', year: 'numeric', day: 'numeric' });
+const logs = glob.sync('./packages/*/CHANGELOG.md');
+
+let markdown = `# Release Notes ${today}\n\n`;
+
+logs.forEach((path) => {
+  const changelog = fs.readFileSync(path, 'utf8');
+
+  if (changelog.indexOf(today) === -1) {
+    return;
+  }
+
+  const packageName = path.split('/')[2];
+  const lines = changelog.split('\n');
+
+  const heading = lines.findIndex((line) => line.indexOf(today) > -1);
+  const version = lines[heading].split(' -')[0];
+
+  const entries = [];
+  let currentLine = heading + 2;
+
+  while (currentLine < lines.length && !(lines[currentLine][0] || '').match(/^[0-9]/)) {
+    entries.push(lines[currentLine]);
+    currentLine += 1;
+  }
+
+  markdown += `## ${packageName} - ${version} \n\n${entries.join('\n\n')}`;
+});
+
+// eslint-disable-next-line no-console
+console.log(markdown);
