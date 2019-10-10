c30e15c9509dae655f80aa5dc68927e14ca17fc6
Naveen Kumar Ramamurthy <nramamur@buffalo.edu>
2019-10-10 12:47:42 +0530

    Initial Skeleton.

---
+++ packages/terra-switch/src/terra-dev-site/doc/example/DefaultSwitch.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+import Switch from 'terra-switch/lib/Switch';
+
+export default () => <Switch />;
